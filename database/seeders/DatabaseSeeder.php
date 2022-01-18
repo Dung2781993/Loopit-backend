b1ded68f49824b143ce0d44a740f0e30354fdfa5
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-01-18 19:06:19 +1100

    Setting up JWT

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use DateTime;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Hellomama',
+            'email' => 'Hellomama@gmail.com',
+            'password' => Hash::make('Hellomama93'),
+            'created_at' => new DateTime()
+        ]);
     }
 }
