c13c536b41e713972f802e31c16db7bc8be9fd34
Dung2781993@gmail.com <Dung2781993@gmail.com>
2022-01-19 19:22:39 +1100

    Return status after login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
      */
 
     public function login(Request $request)
-    {
+    {  
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string|min:6',
@@ -126,7 +126,8 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => auth()->user(),
+            'status' => true
         ]);
     }
 }
